9da7a5040c434e5bc1eccade999cf2c9b8467f49
Roman-Kamko <r.kamko@mail.ru>
2023-10-21 17:01:04 +0400

    readme

--- src/main/java/com/kamko/bankdemo/repo/TransactionRepo.java
+++ src/main/java/com/kamko/bankdemo/repo/TransactionRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 
@@ -13,6 +14,6 @@ public interface TransactionRepo extends JpaRepository<Transaction, Long> {
             from Transaction t
             where t.account.id = :accountId
             """)
-    Page<Transaction> findPageOfTransaction(Pageable pageable, Long accountId);
+    Page<Transaction> findPageOfTransaction(Pageable pageable, @Param(value = "accountId") Long accountId);
 
 }
